de9b50c67b907c77a7c1886aa380111288ae8995
JunwooPark <wnsdn1994@gmail.com>
2024-04-23 17:33:46 +0900

    초기세팅

---
+++ src/main/java/thecommerce/thecommerce/test/TheCommerceTestApplication.java
@@ -0,0 +1,13 @@
+package thecommerce.thecommerce.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCommerceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCommerceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thecommerce/thecommerce/test/TheCommerceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package thecommerce.thecommerce.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
