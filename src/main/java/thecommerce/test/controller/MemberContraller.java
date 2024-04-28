8ed2e9f87e81da3d593e56c769b326b071e2eb3e
JunwooPark <wnsdn1994@gmail.com>
2024-04-28 20:40:15 +0900

    Fix: 회원가입 controller 수정 - 201응답코드

--- src/main/java/thecommerce/test/controller/MemberContraller.java
+++ src/main/java/thecommerce/test/controller/MemberContraller.java
@@ -1,9 +1,13 @@
 package thecommerce.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import thecommerce.test.domain.Member;
 import thecommerce.test.service.MemberService;
 
@@ -19,9 +23,11 @@ public class MemberContraller {
         this.memberService = memberService;
     }
 
-    @GetMapping("/api/user/join")
-    public String cretaeForm() {
-        return "";
+    // 1. 회원가입
+    @PostMapping("/api/user/join")
+    public ResponseEntity<Void> joinMember(@RequestBody Member member) {
+        memberService.join(member);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/api/user/list")
